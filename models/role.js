84f0130f34efcdb88711db1574854361f4c94bf9
Alex Vargas <alexv7142@gmail.com>
2023-11-29 23:52:51 -0600

    The 'description' property  was seted for role mongoose model

--- models/role.js
+++ models/role.js
@@ -4,8 +4,15 @@ const { Schema, model } = require("mongoose");
 const RoleSchema = Schema({
     role: {
         type: String,
+        min: 3,
+        max: 20,
         required: [true, 'The role is required'],
         unique: true
+    },
+    description: {
+        type: String,
+        max: 300,
+        require: [true, 'Please, set a brief description about this role']
     },
     state: {
         type: Boolean,
@@ -18,4 +25,4 @@ RoleSchema.methods.toJSON = function() {
     return data;
 }
 
-module.exports = model('Role', RoleSchema);
+module.exports = model('Role', RoleSchema); 
